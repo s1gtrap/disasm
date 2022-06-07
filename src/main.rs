9670588f678859e99985dd7ad85e58771ad9adf0
William Tange <will@brt.sexy>
2022-06-07 10:57:12 +0200

    Looks stupid but works lmao

--- src/main.rs
+++ src/main.rs
@@ -7,17 +7,30 @@ mod editor;
 #[function_component(App)]
 fn app() -> Html {
     let asm = use_state(|| vec![]);
+    let err = use_state(|| None);
     let onchange = {
         let asm = asm.clone();
+        let err = err.clone();
         Callback::from(move |c: String| {
-            let insts = dasha::text::tokenize(&c).unwrap();
-            log::info!("got onchange: {:?}", insts);
-            asm.set(insts)
+            match (|| {
+                let tokens = dasha::text::tokenize(&c)?;
+                dasha::disasm(tokens)
+            })() {
+                Ok(insts) => {
+                    asm.set(insts);
+                    err.set(None);
+                }
+                Err(e) => {
+                    asm.set(vec![]);
+                    err.set(Some(e));
+                }
+            }
         })
     };
     html! {
         <>
             <h1>{ "Hello World" }</h1>
+            <h5>{ format!("error: {:?}", *err) }</h5>
             <editor::Editor content={"00 00"} {onchange} />
             { format!("{:?}", *asm) }
         </>
